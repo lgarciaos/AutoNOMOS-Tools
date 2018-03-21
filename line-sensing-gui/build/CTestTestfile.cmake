b8fc0a879d431574a62a96c5ffd06b4377ac196e
Edgar Granados <gary.granados@gmail.com>
2018-03-21 09:49:17 -0600

    del build and devel dirs

--- line-sensing-gui/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/egranadoo/AutoNOMOS-Tools/line-sensing-gui/src
-# Build directory: /home/egranadoo/AutoNOMOS-Tools/line-sensing-gui/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("rqt_autonomos_line_calibration")
--- line-sensing-gui/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/egranadoo/AutoNOMOS-Tools/line-sensing-gui/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/opt/ros/kinetic/ros_catkin_ws/devel_isolated/xacro;/opt/ros/kinetic/ros_catkin_ws/install_isolated".split(';') if "/opt/ros/kinetic/ros_catkin_ws/devel_isolated/xacro;/opt/ros/kinetic/ros_catkin_ws/install_isolated" != "" else []
