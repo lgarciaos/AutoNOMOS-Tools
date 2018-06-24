7bde313beba2a6d24f7ef7a8fd3fb3ee591398b8
Edgar Granados Osegueda <gary.granados@gmail.com>
2018-06-24 18:20:41 -0500

    removed build and devel dirs

--- sensors_visualization/build/occupancy_grid_visualization/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/egranadoo/autonomos_project/AutoNOMOS-Tools/sensors_visualization/src/occupancy_grid_visualization
-# Build directory: /home/egranadoo/autonomos_project/AutoNOMOS-Tools/sensors_visualization/build/occupancy_grid_visualization
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
